cd31691c57bb86681b17ed1126331ddeb97ce04e
Janyk Steenbeek <info@janyksteenbeek.nl>
2015-11-04 03:36:29 -0500

    Applied fixes from StyleCI

--- example.php
+++ example.php
@@ -1,7 +1,8 @@
 <?php
+
 require_once 'src/janyksteenbeek/COChannels/COChannels.php';
 
 $contentOwner = new janyksteenbeek\COChannels\COChannels('username@gmail.com', 'myP@$$word', 'contentOwnerID');
 
-$contentOwner->addChannel(array('janyksteenbeek', 'pewdiepie'));
+$contentOwner->addChannel(['janyksteenbeek', 'pewdiepie']);
 $contentOwner->removeChannel('JL2pyjJQ_7VbUKItznyTsQ');
